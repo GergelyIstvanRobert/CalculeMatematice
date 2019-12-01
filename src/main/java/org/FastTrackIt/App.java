485ba7b6a586d9ea44a1a36755fe95e5052dc546
GergelyIstvanRobert <skiridershop@gmail.com>
2019-12-01 10:59:00 +0200

    Calcule

---
+++ src/main/java/org/FastTrackIt/App.java
@@ -0,0 +1,28 @@
+package org.FastTrackIt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        int x = 13;
+        int y = 7;
+        int results;
+
+        results = x+y;
+        System.out.println("The result is " + results);
+        results = x-y;
+        System.out.println("The result is " + results);
+        results = x*y;
+        System.out.println("The result is " + results);
+        results = x/y;
+        System.out.println("The result is " + results);
+
+
+    }
+}
---
+++ src/main/java/org/FastTrackIt/Numere.java
@@ -0,0 +1,6 @@
+package org.FastTrackIt;
+
+public class Numere {
+    int x = 13;
+    int y = 7;
+}
