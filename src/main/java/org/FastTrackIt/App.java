65119d7038207f89fba4371c141c104a2f285649
GergelyIstvanRobert <skiridershop@gmail.com>
2019-12-02 15:15:51 +0200

    Calcule corectat.

--- src/main/java/org/FastTrackIt/App.java
+++ src/main/java/org/FastTrackIt/App.java
@@ -7,8 +7,22 @@ package org.FastTrackIt;
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    { System.out.println( "Hello World!" );
+
+        Numere sumVar = new Numere();
+        sumVar.sumVar(11,11);
+
+        Numere multiply = new Numere();
+        sumVar.multiply(12,2);
+
+        Numere division = new Numere();
+        sumVar.division(333,3);
+
+        Numere subtraction = new Numere();
+        sumVar.subtraction(123,65);
+
+
+
 
         int x = 13;
         int y = 7;
@@ -24,8 +38,7 @@ public class App
         System.out.println("The result is " + results);
 
     }
-    Numere sumVar = new Numere();
-    sumVar.sumVar
+
 
 
 }
--- src/main/java/org/FastTrackIt/Numere.java
+++ src/main/java/org/FastTrackIt/Numere.java
@@ -1,12 +1,25 @@
 package org.FastTrackIt;
 
 public class Numere {
-    int x = 13;
-    int y = 7;
 
     public int sumVar (int x,int y){
         int sumVar = x+y;
-        System.out.println("The sum of this variable is" +sumVar);
+        System.out.println("The sum of this variable is " +sumVar);
+        return sumVar;
+    }
+    public int multiply (int x,int y) {
+        int sumVar = x*y;
+        System.out.println("The sum of this variable is " + sumVar);
+        return sumVar;
+    }
+    public int division (int x,int y) {
+        int sumVar = x/y;
+        System.out.println("The sum of this variable is " +sumVar);
+        return sumVar;
+    }
+    public int subtraction (int x,int y) {
+        int sumVar = x-y;
+        System.out.println("The sum of this variables is " + sumVar);
         return sumVar;
     }
 
